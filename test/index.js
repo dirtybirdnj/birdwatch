d3482e0ea4912f38edbc60284d3a66278d8f8019
Mat Gilbert <matgilbert@gmail.com>
2018-08-26 18:40:39 -0400

    Satisfying eslint errors thrown during tests, updated readme

--- test/index.js
+++ test/index.js
@@ -39,8 +39,45 @@ describe('Drones', () => {
         });
 
         expect(response.statusCode).to.equal(200);
-        
     });
+
+    //This is a unique value that should be created each time the suite runs
+    //So that tests can be run without modifying this file
+    const droneSerial = 'TESTDRN' + Date.now();
+
+    it('should create a drone', async () => {
+
+        const server = await Server.deployment();
+
+        const response = await server.inject({
+            method: 'POST',
+            url: '/drones',
+            payload: {
+                model: 'Parrot Anafi',
+                serial: droneSerial
+            }
+        });
+
+        expect(response.statusCode).to.equal(200);
+    });
+
+    it('should not allow a duplicate drone to be created', async () => {
+
+        const server = await Server.deployment();
+
+        const response = await server.inject({
+            method: 'POST',
+            url: '/drones',
+            payload: {
+                model: 'Parrot Anafi',
+                serial: droneSerial
+            }
+        });
+
+        //Should throw a 400 because a unique constraint is violated by payload
+        expect(response.statusCode).to.equal(400);
+    });
+
 
 
 });
